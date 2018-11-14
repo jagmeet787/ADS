2a3184a380de2f1027ac39bb9ca71a1ae0f87a41
Jagmeet Singh <jagmeet787@gmail.com>
2018-11-14 13:44:54 +0530

    Ugly Numbers

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,9 @@
-
-
-import Algorithms.DynamicProgramming.EggDropping;
+import Algorithms.DynamicProgramming.UglyNumbers;
+import Algorithms.Graph.*;
 
 public class Driver {
 	public static void main(String[] args) {
-		System.out.println(EggDropping.eggDroppingRec(16, 3));
-		System.out.println(EggDropping.eggDroppingDP(16, 4));
-		System.out.println(EggDropping.eggDroppingDPMin(16, 4));
+		System.out.println("20th Ugly number: " + UglyNumbers.nthUglyNumber(200));;
+		System.out.println("20th Ugly number: " + UglyNumbers.nthUglyNumberDP(200));
 	}
 }

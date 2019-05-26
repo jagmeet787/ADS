a07435818dd6e1ebf2a1b66ee01d4333d7d63e1f
Jagmeet Singh <jagmeet787@gmail.com>
2019-05-26 09:02:34 +0530

    Create Triangle.java

---
+++ src/main/java/ADS/Geometry/Triangle.java
@@ -0,0 +1,13 @@
+package ADS.Geometry;
+
+public class Triangle {
+    public Point a, b, c;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a; this.b = b; this.c = c;
+    }
+
+    public double area() {
+        return Math.abs(Point.cross(new Point(a.x - b.x, a.y - b.y), new Point(c.x - a.x, c.y - a.y))) / 2.0;        
+    }
+}

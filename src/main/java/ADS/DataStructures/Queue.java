87c6dfa7e97a081786189083bb7516d4e0ad5b0c
Jagmeet Singh <jagmeet787@gmail.com>
2019-05-28 03:32:43 +0530

    Queue using Array

---
+++ src/main/java/ADS/DataStructures/Queue.java
@@ -0,0 +1,55 @@
+package ADS.DataStructures;
+
+class Queue {
+    int[] arr;
+    int front, back, n;
+    
+    public Queue(int capacity) {
+        this.n = capacity + 1;
+        arr = new int[n];
+        this.front = this.back = 0;
+    }
+    
+    public boolean isEmpty() {
+        return front == back;
+    }
+    
+    public boolean isFull() {
+        return (back + 1) % n == front;
+    }
+    
+    public int size() {
+        if (back < front) return back + n - front;
+        return back - front;
+    }
+    
+    public void enqueue(int element) {
+        if (isFull())
+            throw new RuntimeException("Overflow: Queue is full.");
+        back = (++back) % n;
+        arr[back] = element;
+    }
+    
+    public int dequeue() {
+        if (isEmpty())
+            throw new RuntimeException("Underflow: Queue is empty.");
+        front = (++front) % n;
+        return arr[front];
+    }
+    
+    public int peek() {
+        return arr[(front + 1) % n];
+    }
+    
+    @Override public String toString() {
+        int i = front;
+        StringBuilder out = new StringBuilder();
+        out.append("[");
+        while (i != back) {
+            i = (i + 1) % n;
+            out.append(" " + arr[i]);
+        }
+        out.append(" ]");
+        return out.toString();
+    }
+}

daf367f2332e0ea8f8713f5ed0d7338d745b937b
Jagmeet Singh <jagmeet787@gmail.com>
2019-06-11 15:56:42 +0530

    Iterative gcd

--- src/main/java/ADS/Mathematics/GCD.java
+++ src/main/java/ADS/Mathematics/GCD.java
@@ -2,8 +2,18 @@ package ADS.Mathematics;
 
 class GCD {
 	// Time complexity : O(log(a + b))
-	public static long gcd(long a, long b) {
+	public static long gcd_rec(long a, long b) {
 		if (b == 0) return (a < 0) ? -a : a;
 		return gcd(b, a % b);
+	}
+
+	public static long gcd(long a, long b) {
+		long temp = 0;
+		while (b > 0) {
+			temp = b;
+			b = a % b;
+			a = temp;
+		}
+		return (a < 0) ? -a : a;
 	}
 }
